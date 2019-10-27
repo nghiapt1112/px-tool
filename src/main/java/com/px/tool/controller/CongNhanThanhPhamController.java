d2ad8397fe916cc2166c2d6274accb9560c3cafe
nghiapt1112 <nghiapt1112@gmail.com>
2019-10-27 20:12:42 +0700

    fix: Missing api path

--- src/main/java/com/px/tool/controller/CongNhanThanhPhamController.java
+++ src/main/java/com/px/tool/controller/CongNhanThanhPhamController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("cntp")
+@RequestMapping("/cntp")
 public class CongNhanThanhPhamController extends BaseController {
 
     @Autowired
