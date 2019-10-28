c336ce1efc1808de872380204d0601d83f52d3de
nghiapt1112 <nghiapt1112@gmail.com>
2019-10-28 13:13:58 +0700

    fix cors

--- src/main/java/com/px/tool/PxApplication.java
+++ src/main/java/com/px/tool/PxApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PxApplication {
+public class PxApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PxApplication.class, args);
@@ -18,4 +20,9 @@ public class PxApplication {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
 }
