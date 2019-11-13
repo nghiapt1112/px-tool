2eed21d68e08be9222ad12f45182ac4db607b8fc
nghiapt1112 <nghiapt1112@gmail.com>
2019-11-14 00:07:21 +0700

    fix: thong ke

--- src/main/java/com/px/tool/domain/request/ThongKePayload.java
+++ src/main/java/com/px/tool/domain/request/ThongKePayload.java
@@ -1,5 +1,6 @@
 package com.px.tool.domain.request;
 
+import com.px.tool.domain.dathang.PhieuDatHangDetail;
 import com.px.tool.domain.kiemhong.KiemHongDetail;
 import com.px.tool.infrastructure.model.request.AbstractObject;
 import lombok.Getter;
@@ -7,7 +8,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -34,6 +37,12 @@ public class ThongKePayload extends AbstractObject {
     public static ThongKePayload fromRequestEntity(Request request) {
         List<ThongKePayload> tks = new ArrayList<>();
         ThongKePayload tk = null;
+        Map<Long, PhieuDatHangDetail> pdhDetailById = new HashMap<>();
+        for (PhieuDatHangDetail phieuDatHangDetail : request.getPhieuDatHang().getPhieuDatHangDetails()) {
+            pdhDetailById.put(phieuDatHangDetail.getKiemHongDetailId(), phieuDatHangDetail);
+        }
+
+        PhieuDatHangDetail pdhDt = null;
         for (KiemHongDetail detail : request.getKiemHong().getKiemHongDetails()) {
             //        tk.tt =
             tk = new ThongKePayload();
@@ -47,6 +56,18 @@ public class ThongKePayload extends AbstractObject {
             tk.ngayChuyenPhongVatTu = "11/12/1993";
             // TODO: set them thong tin cua kiem hong id de lay dc data.
             // TODO: cho ra map<kiemhongDetailId, kiemhong>
+
+
+//            tk.tt = Long.valueOf(el);
+            pdhDt = pdhDetailById.get(detail.getKhDetailId());
+            tk.soPhieuDatHang = pdhDt.getSoPhieuDatHang();
+            tk.ngayChuyenKT = request.getPhieuDatHang().getNgayThangNamTPVatTu();
+//            tk.soPA = "So phuong an" + el;
+            tk.ngayRaPA = request.getPhuongAn().getNgayThangNamNguoiLap();
+            tk.ngayChuyenKH = request.getPhuongAn().getNgayThangNamtpVatTu();
+            tk.ngayPheDuyet = request.getPhuongAn().getNgayThangNamTPKTHK();
+//            tk.ngayHoanThanh = request.getCongNhanThanhPham().getNguoiThucHienXacNhan();
+//            tk.xacNhanHoanThanh = "Da hoan thanh";
         }
         return tk;
     }
