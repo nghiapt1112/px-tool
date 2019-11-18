48c1bfab9855b43a67d16375c2d7adfc61502568
nghiapt1112 <nghiapt1112@gmail.com>
2019-11-18 13:38:46 +0700

    Van ban den CRUD

--- src/main/java/com/px/tool/controller/VanBanDenController.java
+++ src/main/java/com/px/tool/controller/VanBanDenController.java
@@ -1,10 +1,14 @@
 package com.px.tool.controller;
 
 import com.px.tool.domain.vanbanden.VanBanDenPayload;
+import com.px.tool.domain.vanbanden.repository.VanBanDenRepository;
 import com.px.tool.domain.vanbanden.service.VanBanDenServiceImpl;
 import com.px.tool.infrastructure.BaseController;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,9 @@ public class VanBanDenController extends BaseController {
     @Autowired
     private VanBanDenServiceImpl vanBanDenService;
 
+    @Autowired
+    private VanBanDenRepository vanBanDenRepository;
+
     @GetMapping
     public List<VanBanDenPayload> list() {
         return vanBanDenService.findAll();
@@ -29,4 +36,13 @@ public class VanBanDenController extends BaseController {
         return vanBanDenService.save(payload);
     }
 
+    @GetMapping("/{id}")
+    public VanBanDenPayload detail(@PathVariable Long id){
+        return vanBanDenService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        vanBanDenService.deleteById(id);
+    }
 }
--- src/main/java/com/px/tool/domain/vanbanden/service/VanBanDenServiceImpl.java
+++ src/main/java/com/px/tool/domain/vanbanden/service/VanBanDenServiceImpl.java
@@ -2,6 +2,7 @@ package com.px.tool.domain.vanbanden.service;
 
 import com.px.tool.domain.vanbanden.VanBanDenPayload;
 import com.px.tool.domain.vanbanden.repository.VanBanDenRepository;
+import com.px.tool.infrastructure.exception.PXException;
 import com.px.tool.infrastructure.service.impl.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,21 @@ public class VanBanDenServiceImpl extends BaseServiceImpl {
     @Transactional
     public VanBanDenPayload save(VanBanDenPayload payload) {
         return VanBanDenPayload.fromEntity(vanBanDenRepository.save(payload.toEntity()));
+    }
+
+    public VanBanDenPayload findById(Long id) {
+        return VanBanDenPayload.fromEntity(
+                vanBanDenRepository.findById(id)
+                        .orElseThrow(() -> new PXException("vanbanden_notFound"))
+        );
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        try {
+            vanBanDenRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new PXException("vanbanden_deleteFailed");
+        }
     }
 }
